3e9fa83de1eed5d74832647daeb85a9e1c5a448f
Ashu Goyal <ashu125001@gmail.com>
2018-01-20 01:24:01 +0530

    changed

--- splitString.py
+++ splitString.py
@@ -1,4 +1,4 @@
 a = 'yellow green red blue white pink brown magenta'#here make a string
 a.split()# here spliting the string
 x = a.split() #here output is stored in x
-print(x)
+print(x)#print the x 
